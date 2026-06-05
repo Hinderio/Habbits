b5e86ba78c7acb465603fd739ad8f532ee587f4c
Hinderio <128827870+Hinderio@users.noreply.github.com>
2026-06-05 19:40:03 +0200

    Restore working Supabase anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.HABITFLOW_SUPABASE_CONFIG = Object.freeze({
   url: 'https://spzytdyottsicwmmwsbl.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzcHp5dGR5b3R0c2ljd21td3NibCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzc2NTQxOTM5LCJleHAiOjIwOTIxMTc5Mzl9.9Ku9KU102YaX1UhWVPntA6q7vzyvo7rCzgn68pLS9xU'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwenl0ZHlvdHRzaWN3bW13c2JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NDE5MzksImV4cCI6MjA5MjExNzkzOX0.9Ku9KU102YaX1UhWVPntA6q7vzyvo7rCzgn68pLS9xU'
 });
 
 (function routeTopbarSyncToSettings(document) {
